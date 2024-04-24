2385ea41b261140a92bb3ab7c43eb0c5601964b8
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2024-04-24 08:46:45 +0200

    OrderSummary - Darkmode fix + minor style changes.

--- src/components/OrderSummary.jsx
+++ src/components/OrderSummary.jsx
@@ -1,4 +1,4 @@
-function OrderSummary({ orderItems, incrementQuantity, decrementQuantity }) {
+function OrderSummary({ orderItems, darkMode, incrementQuantity, decrementQuantity }) {
   const calculateTotal = (orderItems) => {
     return orderItems.reduce(
       (total, item) => total + item.price * item.quantity,
@@ -7,13 +7,13 @@ function OrderSummary({ orderItems, incrementQuantity, decrementQuantity }) {
   };
 
   return (
-    <div className="fixed-bottom bg-light p-1 mx-4">
+    <div className={`fixed-bottom p-1 mx-4 bg-${darkMode ? "dark": "light"}`}>
       <h3>Order Summary</h3>
       <table className="table table-hover">
         <thead>
           <tr>
             <th>Item</th>
-            <th>Quantity</th>
+            <th className="text-center">Quantity</th>
             <th>Price</th>
             <th>Total</th>
           </tr>
@@ -22,36 +22,34 @@ function OrderSummary({ orderItems, incrementQuantity, decrementQuantity }) {
           {orderItems.map((item, index) => (
             <tr key={index}>
               <td>{item.title}</td>
-              <td>
+              <td className="text-center">
                 <button
                   type="button"
-                  className="btn btn-outline-danger btn-sm"
+                  className="btn btn-outline-danger btn-sm px-3 font-monospace"
                   onClick={() => decrementQuantity(item)}
                 >
-                  {" "}
-                  -{" "}
+                  -
                 </button>
                 <span style={{ marginRight: "8px", marginLeft: "8px" }}>
                   {item.quantity}
                 </span>
                 <button
-                  className="btn btn-outline-success btn-sm"
+                  className="btn btn-outline-success btn-sm px-3 font-monospace"
                   onClick={() => incrementQuantity(item)}
                 >
-                  {" "}
-                  +{" "}
+                  +
                 </button>
               </td>
-              <td>SEK{item.price.toFixed(2)}</td>
-              <td>SEK{(item.price * item.quantity).toFixed(2)}</td>
+              <td>{item.price.toFixed(2)}</td>
+              <td>{(item.price * item.quantity).toFixed(2)}</td>
             </tr>
           ))}
-          <tr>
-            <td colSpan="3" className="font-weight-bold text-right">
-              Total:
+          <tr className="fw-bold">
+            <td colSpan="3" className="text-end">
+               Total Price:
             </td>
-            <td className="font-weight-bold">
-              SEK{calculateTotal(orderItems).toFixed(2)}
+            <td>
+              {calculateTotal(orderItems).toFixed(2)} SEK
             </td>
           </tr>
         </tbody>
